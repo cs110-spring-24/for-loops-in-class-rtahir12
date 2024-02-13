3c4673becb1bc8361ceaaec4bfb36189bcbcde48
rtahir12 <157747924+rtahir12@users.noreply.github.com>
2024-02-13 15:32:15 -0800

    Update multiples.py

---
+++ multiples.py
@@ -0,0 +1,17 @@
+
+# Define the range (1 to 100 for example)
+range_start = 1
+range_end = 100
+
+# Initialize lists to store multiples of 3 and 5
+multiples_of_3 = []
+multiples_of_5 = []
+
+for number in range(range_start, range_end + 1):
+    if number 3 == 0:
+        multiples_of_3.(number)
+    if number 5 == 0:
+        multiples_of_5.(number)
+
+# Output the results
+ multiples_of_5
